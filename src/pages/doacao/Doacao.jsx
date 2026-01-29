fac15b843b2bae9c6421c46086eac903a7396a78
RicardoFrancoDelgado <rf041555@gmail.com>
2026-01-29 15:34:50 -0300

    feat: concluded donate screen

--- src/pages/doacao/Doacao.jsx
+++ src/pages/doacao/Doacao.jsx
@@ -1,6 +1,8 @@
 import Card from "../../components/card/card";
 import styles from "./doacao.module.scss";
 import doacao from "../../assets/images/doacao-projeto.png";
+import livros from "../../assets/images/doe-livros.png";
+import computador from "../../assets/images/doe-computador.png";
 
 export function Doacao() {
   return (
@@ -14,16 +16,18 @@ export function Doacao() {
           description="Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade."
           btnTitle="Quero Doar"
         />
+        <div className={styles.divBar} />
         <Card
-          image={doacao}
-          title="Instituto grande familia"
-          description="Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade."
+          image={livros}
+          title="Projeto Futuro na Escolafamilia"
+          description="Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a continuarem seus estudos com mais estrutura"
           btnTitle="Quero Doar"
         />
+        <div className={styles.divBar} />
         <Card
-          image={doacao}
-          title="Instituto grande familia"
-          description="Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade."
+          image={computador}
+          title="Instituto Conecta Jovem"
+          description="Doe computadores usados, tablets ou celulares em bom estado para permitir que jovens tenham acesso ao mundo digital e novas oportunidades"
           btnTitle="Quero Doar"
         />
       </div>
