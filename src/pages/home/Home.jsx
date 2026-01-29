89ff5a1d23199fdc989f55128d4fb60c796a31dd
RicardoFrancoDelgado <rf041555@gmail.com>
2026-01-29 08:15:25 -0300

    feat: ajustando page home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,47 +5,45 @@ import styles from "./home.module.scss";
 
 export function Home() {
   return (
-    <>
-      <section className={styles.section}>
-        <div className={styles.textContent}>
-          <h1>Projetos Sociais que transformam</h1>
-          <p>
-            Conectamos sua empres com projetos sociais impactantes. Juntos,
-            criamos mudanças reais na comunidade
-          </p>
-
-          <div>
-            <a href="" className={styles.cadastrarEmpresa}>
-              Cadastrar Empresa <img src={arrow} alt="Icone de uma seta" />
-            </a>
-          </div>
-
-          <section className={styles.cardSection}>
-            <div className={styles.cards}>
-              <h2>500+</h2>
-              <p>Empresas Voluntárias</p>
-            </div>
-
-            <div className={styles.cards}>
-              <h2>1.2K+</h2>
-              <p>Projetos Realizados</p>
-            </div>
-
-            <div className={styles.cards}>
-              <h2>50K+</h2>
-              <p>Vidas Impactadas</p>
-            </div>
-          </section>
-        </div>
+    <section className={styles.section}>
+      <div className={styles.textContent}>
+        <h1>Projetos Sociais que transformam</h1>
+        <p>
+          Conectamos sua empres com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade
+        </p>
 
         <div>
-          <img
-            src={primary}
-            alt="Imagem de duas mãos segurando bonecos de papel e um globo terrestre"
-            className={styles.primaryImage}
-          />
+          <a href="" className={styles.cadastrarEmpresa}>
+            Cadastrar Empresa <img src={arrow} alt="Icone de uma seta" />
+          </a>
         </div>
-      </section>
-    </>
+
+        <section className={styles.cardSection}>
+          <div className={styles.cards}>
+            <h2>500+</h2>
+            <p>Empresas Voluntárias</p>
+          </div>
+
+          <div className={styles.cards}>
+            <h2>1.2K+</h2>
+            <p>Projetos Realizados</p>
+          </div>
+
+          <div className={styles.cards}>
+            <h2>50K+</h2>
+            <p>Vidas Impactadas</p>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <img
+          src={primary}
+          alt="Imagem de duas mãos segurando bonecos de papel e um globo terrestre"
+          className={styles.primaryImage}
+        />
+      </div>
+    </section>
   );
 }
